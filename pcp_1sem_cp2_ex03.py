2f78af6635b5055463aa4628a7d643ef9cb90f3e
Lucas Ramos de Sousa <146573018+LuRSousa@users.noreply.github.com>
2026-04-17 08:26:39 -0300

    especificando comentário ex3

--- pcp_1sem_cp2_ex03.py
+++ pcp_1sem_cp2_ex03.py
@@ -1,11 +1,13 @@
 #03. Sistema de calculo de médias do aluno
-#Definindo as variáveis
+
+#Definindo as variáveis das notas
 cp1 = (float(input ("Digite qual é sua nota da cp1: ")))
 cp2 = (float(input ("Digite qual é sua nota da cp2: ")))
 cp3 = (float(input ("Digite qual é sua nota da cp3: ")))
 sp1 = (float(input ("Digite qual é sua nota da sp1: ")))
 sp2 = (float(input ("Digite qual é sua nota da sp2: ")))
 gs = (float(input ("Digite qual é sua nota da gs: ")))
+
 #Descartando a menor nota
 menor = 0
 
@@ -15,9 +17,11 @@ elif cp2 < cp1  and cp2 < cp3:
     menor = cp2
 elif cp3 < cp1 and cp3 <cp2:
     menor = cp3
-#Calculos da média do aluno
+    
+#Calculo da média do aluno
 media = ((cp1 + cp2 + cp3 - menor + sp1 + sp2) / 4) * 0.4 + gs * 0.6
-#mostrando resultados
+
+#Mostrando resultados
 print("Essa é sua média:")
 print(f"{media:.1f}")
 
